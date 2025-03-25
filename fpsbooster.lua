6540cdf585e67f67c2bce86db504a5db3ec8a76b
poisonhash <hone45.ugphone@gmail.com>
2025-03-24 20:48:09 -0400

    fpsbooster

---
+++ fpsbooster.lua
@@ -0,0 +1,39 @@
+local Players = game:GetService("Players")
+
+local function cleanCharacter(character)
+    for _, child in ipairs(character:GetChildren()) do
+        if child:IsA("Accessory") then
+            child:Destroy()
+        end
+    end
+
+    for _, descendant in ipairs(character:GetDescendants()) do
+        if descendant:IsA("ParticleEmitter") or
+           descendant:IsA("Beam") or
+           descendant:IsA("Trailend) or
+           descendant:IsA("Fire") or
+           descendant:IsA("Smoke") or
+           descendant:IsA("Sparkles") then
+            descendant:Destroy()
+        
+        elseif descendant:IsA("Decal") then
+            descendant.Texture = ""
+        elseif descendant:IsA("MeshPart") then
+            descendant.TextureID = ""
+        elseif descendant:IsA("SpecialMesh") then
+            descendant.TextureId = ""
+        elseif descendant:IsA("ShirtGraphic") then
+            descendant.Graphic = ""
+        elseif descendant:IsA("Shirt") then
+            descendant.ShirtTemplate = ""
+        elseif descendant:IsA("Pants") then
+            descendant.PantsTemplate = ""
+        end
+    end
+end
+
+Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        cleanCharacter(character)
+    end)
+end)
